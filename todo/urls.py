ec1273275baf24dd98b6558dd10460bc49086de8
chadha <chadha.hadji@gmail.com>
2021-03-17 23:09:29 +0100

    nothing much

---
+++ todo/urls.py
@@ -0,0 +1,29 @@
+"""todo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from todoapp.views import index, about, delete_task, complete_task, delete_completed, delete_all
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name = "index"),
+    path('about', about, name = "aboutpage"),
+    path('delete/<int:task_id>', delete_task , name = "delete"),
+    path('complete/<int:task_pk>', complete_task, name ="complete"),
+    path('delete_completed', delete_completed, name ="delete_completed"),
+    path('delete_all', delete_all, name ="delete_all")
+    
+]
---
+++ todoapp/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+# Create your views here.
+
+def index(request):
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    context = {'all_tasks': tasks,
+               'task_form': form,}
+
+    return render(request, 'todoapp/index.html', context)
+
+
+def about(request):
+    return render(request, 'todoapp/about.html')
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('index')
+
+def complete_task(request, task_pk):
+    task2 = Task.objects.get(id=task_pk)
+    task2.completed = True
+    task2.save()
+    return redirect('index')
+
+def delete_completed(request):
+    completed_tasks = Task.objects.filter(completed = True)
+    completed_tasks.delete()
+    return redirect('index')
+
+def delete_all(request):
+    tasks = Task.objects.all()
+    tasks.delete()
+    return redirect('index')
